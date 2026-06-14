ff8e75306b4352ed0e509873830ef40887c965d6
Ayushman <ayushmanpanda91@gmail.com>
2026-06-14 23:53:05 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.print("$ ");
+        Scanner scanner = new Scanner(System.in);
+        
+        while (true) {
+            System.out.print("$ ");
+            System.out.flush();
+            
+            String input = scanner.nextLine().trim();
+            
+            System.out.println(input + ": command not found");
+        }
     }
 }
