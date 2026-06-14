47b34ae137a782309da651f8513fa26a913148a4
Ayushman <ayushmanpanda91@gmail.com>
2026-06-15 00:00:37 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,9 +12,11 @@ public class Main {
             
             if (input.equals("exit") || input.equals("exit 0")) {
                 System.exit(0);
+            } else if (input.startsWith("echo ")) {
+                System.out.println(input.substring(5));
+            } else {
+                System.out.println(input + ": command not found");
             }
-            
-            System.out.println(input + ": command not found");
         }
     }
 }
